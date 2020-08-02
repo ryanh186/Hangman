ec0dfb92cda003c928cb1ec074d23db3538e7836
jkarime <56406006+jkarime@users.noreply.github.com>
2020-08-02 15:48:43 -0500

    Length of word to underscores

--- driver.py
+++ driver.py
@@ -2,6 +2,11 @@ from getpass import getpass
 
 def main():
     user1input = getpass("Welcome to Hangman!\nUser 1, please enter keyword: ")
+    num_underscore = user1input.len()
+    
+    for i in num_underscore:
+        print(" _ ")
+
 
 if __name__ == "__main__":
     main()
