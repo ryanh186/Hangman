907ac669c92bf714bd71f4d772519228e3fd1428
maamari <maamari@usc.edu>
2020-08-02 15:21:08 -0500

    add hidden input

--- driver.py
+++ driver.py
@@ -1,5 +1,7 @@
+from getpass import getpass
+
 def main():
-    user1input = input("Welcome to Hangman!\nUser 1, please enter keyword: ")
+    user1input = getpass("Welcome to Hangman!\nUser 1, please enter keyword: ")
 
 if __name__ == "__main__":
     main()
