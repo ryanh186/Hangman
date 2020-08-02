8963cd3463c533bf7815b76648c1c56fb794f5c3
Ryan <rhassoun186@gmail.com>
2020-08-02 16:40:15 -0500

    Update driver.py
    test through git hub app

--- driver.py
+++ driver.py
@@ -12,7 +12,7 @@ def main():
     for i in num_underscore:
         print('_ ', end='')
     print('')
-    user2input = input("User2, please enter guess: ")
+    user2input = input("User2, please enter gpenia: ")
 
 
 if __name__ == "__main__":
