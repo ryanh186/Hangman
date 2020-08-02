99dc347b6051aa70c78b2665766f844e69077f85
jkarime <56406006+jkarime@users.noreply.github.com>
2020-08-02 16:20:11 -0500

    Dumb len() mistake

--- driver.py
+++ driver.py
@@ -7,7 +7,7 @@ from getpass import getpass
 
 def main():
     user1input = getpass("Welcome to Hangman!\nUser 1, please enter keyword: ")
-    num_underscore = user1input.len()
+    num_underscore = len(user1input)
     
     for i in num_underscore:
         print(" _ ")
