a9d89416355deaebc9b4c40db28041cc794569b1
maamari <maamari@usc.edu>
2020-08-02 15:16:43 -0500

    working on input

--- driver.py
+++ driver.py
@@ -1,6 +1,5 @@
 def main():
-    print("Welcome to Hangman!")
-    user1input = input("User 1, please enter keyword: ")
+    user1input = input("Welcome to Hangman!\nUser 1, please enter keyword: ")
 
 if __name__ == "__main__":
     main()
